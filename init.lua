6149121bdcc19aa99876d4d8fe3862f79b6eb026
Christoph Otten genannt Hermes <christoph.hermes@tum.de>
2026-03-03 23:31:44 +0100

    added NerdTree on startup

--- init.lua
+++ init.lua
@@ -224,6 +224,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Open NERDTree on vim start
+vim.api.nvim_create_autocmd('VimEnter', { command = 'NERDTree' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
